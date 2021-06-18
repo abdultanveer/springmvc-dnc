e9f5c98bc51217ea716486e3ba587e47946a27ab
abdul <abdul.tanveer@gmail.com>
2021-06-18 15:28:41 +0530

    getting value from model to html page

--- MVCspringboot/src/main/java/com/dnc/mvc/springboot/MainController.java
+++ MVCspringboot/src/main/java/com/dnc/mvc/springboot/MainController.java
@@ -13,13 +13,14 @@ public class MainController {
     @RequestMapping("/")
     public String index(
         @RequestParam(value = "participant", required = false) String participant,
+        @RequestParam(value = "country", required = false) String country,
+        @RequestParam(value = "action", required = false) String action,
+        @RequestParam(value = "id", required = false) Integer id,
         Model model
     ) {
-    	
-       // model.addAttribute("id", id);
-        //List<Integer> userIds = Arrays.asList(1,2,3,4);
-       // model.addAttribute("userIds", userIds);
-        System.out.println(participant);
+        model.addAttribute("id", id);
+        List<Integer> userIds = Arrays.asList(1,2,3,4);
+        model.addAttribute("userIds", userIds);
         return "index";
     }
 }
